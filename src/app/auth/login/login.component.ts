9bb8bd467db1ce175c4f278c15a6c5508772cafa
alexFiorenza <alexxdfiorenza@gmail.com>
2020-10-30 00:38:45 -0300

    Loader in login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { UserService } from './../../core/services/http/api/user/user.service';
 import { environment } from './../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
@@ -12,6 +12,9 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   public form: FormGroup;
+  @ViewChild('btnSubmit') private btnSubmit: ElementRef;
+  @ViewChild('loader') private loader: ElementRef;
+  @ViewChild('textBtn') private textBtn: ElementRef;
   constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
@@ -23,10 +26,14 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
   onSubmit() {
-    const dataToSend = this.form.value;
-    this.userService.logIn(dataToSend).subscribe(data => {
-      this.userService.saveUserSession(data.payload, data.token);
-      this.router.navigate(['/inicio']);
-    });
+    this.textBtn.nativeElement.classList.add('hidden');
+    this.loader.nativeElement.classList.remove('hidden');
+    this.btnSubmit.nativeElement.classList.add('changeWidth');
+
+    // const dataToSend = this.form.value;
+    // this.userService.logIn(dataToSend).subscribe(data => {
+    //   this.userService.saveUserSession(data.payload, data.token);
+    //   this.router.navigate(['/inicio']);
+    // });
   }
 }
