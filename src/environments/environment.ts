f8cffea51f1fb915fdf612cce7e87ef6f9f57987
alexFiorenza <alexxdfiorenza@gmail.com>
2020-11-09 23:39:56 -0300

    Changed environment variable in products service and changed backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.13:3000/api',
-  uploadsUrl: 'http://192.168.0.13:3000/public'
+  apiUrl: "http://localhost:3000/api",
+  uploadsUrl: "http://localhost:3000/public",
 };
 
 /*
