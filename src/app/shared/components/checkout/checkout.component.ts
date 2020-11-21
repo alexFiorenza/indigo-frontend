464e33b65d987e74a08ff90d1efab09fb29bfe9e
alexFiorenza <alexxdfiorenza@gmail.com>
2020-11-20 21:35:22 -0300

    DocType and docNumber inputs added to form

--- src/app/shared/components/checkout/checkout.component.ts
+++ src/app/shared/components/checkout/checkout.component.ts
@@ -1,32 +1,32 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, OnInit, Renderer2, AfterContentInit, Input, Inject } from '@angular/core';
+import { Component, OnInit, Renderer2, AfterContentInit, Input, Inject, DoCheck } from '@angular/core';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit, AfterContentInit {
+export class CheckoutComponent implements OnInit {
   @Input() public price: Number = 100;
+  public mp;
+  private mpPublickKey: string = 'TEST-670fd7e7-cb6d-4220-944e-95c0e38825cd';
   constructor(private r: Renderer2, @Inject(DOCUMENT) public document) {
     this.r.setStyle(document.body, 'background-color', ' #f3f3f3');
 
   }
   ngOnInit(): void {
+    this.generateScript();
   }
-  ngAfterContentInit() {
-  }
-  onBuy() {
 
-  }
-  async generateBtn() {
-    const s = this.r.createElement('script');
-    s.type = 'text/javascript';
-    s.src = 'https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js';
-    s.text = ``;
-    this.r.appendChild(document.body, s);
-    // window.Mercadopago.setPublishableKey("TEST-670fd7e7-cb6d-4220-944e-95c0e38825cd");
-    // console.log(window.Mercadopago)
-
-
+  generateScript() {
+    const script: HTMLScriptElement = this.r.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js';
+    script.text = ``;
+    this.r.appendChild(document.body, script);
+    script.onload = () => {
+      this.mp = (window as any).Mercadopago;
+      this.mp.setPublishableKey(this.mpPublickKey);
+      this.mp.getIdentificationTypes();
+    };
   }
 }
